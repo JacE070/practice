96ba849bea95c4de0485f908bd31fa81650b4214
jace070 <junpingl@outlool.com>
2022-07-09 23:48:55 -0700

    Finish Setting Up DB

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -35,7 +35,7 @@ class Customer(models.Model):
         (MEMBERSHIP_SILVER, 'Silver'),
         (MEMBERSHIP_GOLD, 'Gold'),
     ]
-    given_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
@@ -46,7 +46,7 @@ class Customer(models.Model):
     class Meta:
         db_table = 'store_customers'
         indexes = [
-            models.Index(fields=['last_name', 'given_name'])
+            models.Index(fields=['last_name', 'first_name'])
         ]
 
 
